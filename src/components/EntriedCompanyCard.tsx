347f4d76df96287c27dbe00d633bbb108b20a50a
homan02020608 <homan090402@gmail.com>
2025-06-18 19:35:25 +0900

    changed Link href

--- src/components/EntriedCompanyCard.tsx
+++ src/components/EntriedCompanyCard.tsx
@@ -11,7 +11,7 @@ const EntriedCompanyCard = ({ company_id ,company_name , created_at, deadline , 
 
     return (
         <div className='flexCenter flex-row p-4 m-4 border-2 gap-10 border-gray-400 rounded-3xl  '>
-            <Link href={`/company/${id}`} className='flexCenter whitespace-nowrap rounded-2xl hover:scale-105 hover:transition-transform hover:cursor-pointer'>{company_name}</Link>
+            <Link href={`/company/${company_id}`} className='flexCenter whitespace-nowrap rounded-2xl hover:scale-105 hover:transition-transform hover:cursor-pointer'>{company_name}</Link>
             <div className='flexCenter flex-col'>
                 <p>エントリーシート締切:{deadline?.toLocaleDateString('ja-JP')}</p>
                 <Calendar
