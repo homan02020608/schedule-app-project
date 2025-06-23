fc68a77926b1903bc9ce0318d7d026e09ea614a0
homan02020608 <homan090402@gmail.com>
2025-06-24 01:05:05 +0900

    added filteredCompanies function

--- src/components/EntriedCompanyList.tsx
+++ src/components/EntriedCompanyList.tsx
@@ -1,20 +1,27 @@
+
 import { auth } from '@clerk/nextjs/server'
-import React from 'react'
 import { fetchCompanyListData } from '../../firebase/firebaseFunction'
 import EntriedCompanyCard from './EntriedCompanyCard'
 import { CompanyList } from '@/app/Types'
 import FilterButtons from './FilterButtons'
 
-const EntriedCompanyList = async () => {
+
+const EntriedCompanyList = async (props: { currentStatus: string }) => {
+
     const { userId } = await auth()
-    const companyData = await fetchCompanyListData(userId)
+    const companyData: CompanyList[] = await fetchCompanyListData(userId)
+    const currentStatus = props.currentStatus || 'in_progress';
+
+    const filteredCompanies = companyData.filter(company => {
+        return company.status === currentStatus
+    })
     
     return (
         <div className='flexCenter flex-col border-2 m-4 '>
             <h1 className='font-light text-2xl border-b-2 p-2 m-4'>企業一覧</h1>
-            <FilterButtons/>
+            <FilterButtons />
             <div>
-                {companyData.map((data: CompanyList) => (
+                {filteredCompanies.map((data: CompanyList) => (
                     <div key={data.id}>
                         <EntriedCompanyCard
                             company_id={data.company_id}
