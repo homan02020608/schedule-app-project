ca49db5eb9905efbc2e32141556179572310ea7b
homan02020608 <homan090402@gmail.com>
2025-06-24 01:03:04 +0900

    added search currentStatus params

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@ import AddCompanyForm from "@/components/AddCompanyForm";
 import EntriedCompanyList from "@/components/EntriedCompanyList";
 
 
-export default function Home() {
+
+export default async function Home({searchParams} : {searchParams : Promise<{[key :string] : string | string[] | undefined}>}) {
+  const currentStatus = (await searchParams).status
+  
   return (
     <div className="max-container">
       <h1 className="flex justify-center items-center text-3xl font-light">Schedule-App</h1>
       <AddCompanyForm/>
-      <EntriedCompanyList/>
+      <EntriedCompanyList currentStatus={String(currentStatus)}/>
     </div>
   );
 }
