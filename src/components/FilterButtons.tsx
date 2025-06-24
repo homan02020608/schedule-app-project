04f6cd8f9bca1f7582ce531abb6c95bc1a04e629
homan02020608 <homan090402@gmail.com>
2025-06-24 22:32:23 +0900

    fixed FilterButton default all companies bug

--- src/components/FilterButtons.tsx
+++ src/components/FilterButtons.tsx
@@ -1,45 +1,54 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from './ui/button'
 import { useRouter, useSearchParams } from 'next/navigation'
+import AnimationHover from './AnimationHover';
 
 type FilterStatus = 'interested' | 'in_progress' | 'completed';
 
-const FilterButtons = () => {
+const FilterButtons = (props: { currentStatus: string }) => {
     const router = useRouter();
     const searchParams = useSearchParams()
+    const currentStatus = props.currentStatus;
 
-
-    const handleFilterClick = (status : FilterStatus) => {
+    const handleFilterClick = (status: FilterStatus) => {
         const params = new URLSearchParams(searchParams.toString());
-        params.set('status' , status)
+        params.set('status', status)
         router.push(`?${params.toString()}`);
     }
 
 
     return (
         <div className='flex gap-10'>
-            <Button
-                variant="secondary"
-                className='text-xl font-light px-4 py-2 hover:cursor-pointer '
-                onClick={() => handleFilterClick('interested')}
-            >
-                気になる
-            </Button>
-            <Button
-                variant="secondary"
-                className='text-xl font-light px-4 py-2 hover:cursor-pointer'
-                onClick={() => handleFilterClick('in_progress')}
-            >
-                選考中
-            </Button>
-            <Button
-                variant="secondary"
-                className='text-xl font-light px-4 py-2 hover:cursor-pointer'
-                onClick={() => handleFilterClick('completed')}
-            >
-                選考終了
-            </Button>
+            <AnimationHover>
+                <Button
+                    variant="ghost"
+                    className={`text-xl font-extralight px-4 py-2 hover:cursor-pointer rounded-none ${currentStatus === 'interested' && 'bg-cyan-50'}`}
+                    onClick={() => handleFilterClick('interested')}
+                >
+                    気になる
+
+                </Button>
+            </AnimationHover>
+            <AnimationHover>
+                <Button
+                    variant="ghost"
+                    className={`text-xl font-extralight px-4 py-2 hover:cursor-pointer rounded-none ${currentStatus === 'in_progress' && 'bg-cyan-50'}`}
+                    onClick={() => handleFilterClick('in_progress')}
+
+                >
+                    選考中
+                </Button>
+            </AnimationHover>
+            <AnimationHover>
+                <Button
+                    variant="ghost"
+                    className={`text-xl font-extralight px-4 py-2 hover:cursor-pointer rounded-none ${currentStatus === 'completed' && 'bg-cyan-50'}`}
+                    onClick={() => handleFilterClick('completed')}
+                >
+                    選考終了
+                </Button>
+            </AnimationHover>
         </div>
     )
 }
