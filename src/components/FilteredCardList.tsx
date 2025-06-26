fcd5c1caae886c9c89a241e66135dd54a9f69d5a
homan02020608 <homan090402@gmail.com>
2025-06-26 22:33:28 +0900

    added FilteredCardList Components

---
+++ src/components/FilteredCardList.tsx
@@ -0,0 +1,38 @@
+import { CompanyList } from '@/app/Types'
+import React from 'react'
+import EntriedCompanyCard from './EntriedCompanyCard'
+
+interface CompanyDataProps {
+    companyData: CompanyList[]
+    currentStatus: string
+}
+
+const FilteredCardList = ({ companyData, currentStatus }: CompanyDataProps) => {
+    const filteredCompanies = companyData.filter(company => {
+        if (currentStatus === 'all') {
+            return true;
+        }
+        return company.status === currentStatus
+    })
+
+    return (
+        <div>
+            {filteredCompanies.map((company) => (
+                <div key={company.id}>
+                    <EntriedCompanyCard
+                        company_id={company.company_id}
+                        company_name={company.company_name}
+                        created_at={company.created_at?.toDate()}
+                        deadline={company?.deadline?.toDate()}
+                        //id={data.id}
+                        details={company.details}
+                        occupation={company.occupation}
+                        status={company.status}
+                    />
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default FilteredCardList
